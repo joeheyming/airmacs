37492db0b81644ac4384a24a9c59de9506c1112d
joeheyming <joeheyming@gmail.com>
2014-09-18 23:28:02 -0700

    added update buffers, moved things around in .emacs

--- .elisp/utils.el
+++ .elisp/utils.el
@@ -586,3 +586,28 @@ The characters copied are inserted in the buffer before point."
     (progn
       (set-buffer-modified-p 't)
       (save-buffer))))
+
+
+(defun util-update-buffers ()
+  "Refreshs all open buffers from their respective files"
+  (interactive)
+  (let* ((list (buffer-list)) (buffer (car list)) errmesg)
+    (loop for buffer in (buffer-list) do
+          (if (and (not (string-match "\\*" (buffer-name buffer)))
+                   (buffer-file-name buffer)
+                   (file-exists-p (buffer-file-name buffer)))
+              (if (and (not (verify-visited-file-modtime buffer)) ; been touched
+                       (buffer-modified-p buffer)) ; and modified 
+                  (setq errmesg (concat errmesg 
+                    (format "Buffer '%s' has file and buffer changes!\n" buffer)))
+                (util-update-buffer buffer))))
+    (if errmesg (setq errmesg (chomp errmesg)))
+    (message "%s" (or errmesg "Done refreshing all open non-modified files..."))))
+
+(defun util-update-buffer (buffer)
+  (set-buffer buffer) 
+  (message "Refreshing %s" (buffer-file-name buffer))
+  (if (not (verify-visited-file-modtime buffer))
+      (if (buffer-modified-p buffer) (error "Buffer has file and buffer changes")
+        (revert-buffer t t t))) ; revert if touched and not modified
+  (vc-file-clearprops (buffer-file-name)))
